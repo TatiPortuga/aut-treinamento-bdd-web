092d67b02c6aadd7103a90437c3345b02cf18f40
Tatiana Taketsuma <tatiana_taketsuma185@hotmail.com>
2019-03-14 09:10:10 -0300

    Estrutura do Projeto

---
+++ src/test/java/curso/treinamento/runners/RunnerTest.java
@@ -0,0 +1,13 @@
+package curso.treinamento.runners;
+
+import org.junit.runner.RunWith;​
+import cucumber.api.CucumberOptions;​
+import cucumber.api.junit.Cucumber;​
+
+@RunWith(Cucumber.class)​
+@CucumberOptions(	​
+    features = { "src/test/resources/features/"},            ​
+    glue = {"curso.treinamento.steps”,curso.treinamento.setup"}, ​
+    tags = {}) ​
+
+public class RunnerTest {}
